012000bd870e1c082a7874f2c47f5e1000157a23
Kenichi Ishigaki <ishigaki@cpan.org>
2014-07-13 02:00:06 +0900

    dropped experimental Test::More 1.5/2.0 support

--- lib/Test/UseAllModules.pm
+++ lib/Test/UseAllModules.pm
@@ -49,13 +49,7 @@ READ:
 }
 
 sub _planned {
-  if ($Test::More::VERSION >= 2) {
-    Test::More->builder->_plan_handled;
-  } elsif ($Test::More::VERSION >= 1.005) {
-    Test::More->builder->history->plan;
-  } else {
-    Test::More->builder->{Have_Plan};
-  }
+  Test::More->builder->has_plan;
 }
 
 sub all_uses_ok {
